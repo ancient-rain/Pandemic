cf8671de901be0e51ca2ef93b3395ea2c4ce2054
askalerion <bednartd@rose-hulman.edu>
2017-05-09 04:20:12 -0400

    Cure markers drawn

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -14,18 +14,18 @@ public class Constants {
 	public final static int OFFSET_30 = 30;
 	
 	public final static String MAP_IMG = "/images/map.png";
-	public final static String BLACK_OUTLINE = "images/cureMarkers/blackOutline.png";
-	public final static String BLUE_OUTLINE = "images/cureMarkers/blueOutline.png";
-	public final static String RED_OUTLINE = "images/cureMarkers/redOutline.png";
-	public final static String YELLOW_OUTLINE = "images/cureMarkers/yellowOutline.png";
-	public final static String BLACK_CURED = "images/cureMarkers/black.png";
-	public final static String BLUE_CURED = "images/cureMarkers/blue.png";
-	public final static String RED_CURED = "images/cureMarkers/red.png";
-	public final static String YELLOW_CURED = "images/cureMarkers/yellow.png";
-	public final static String BLACK_ERADICATED = "images/cureMarkers/blackEradicated.png";
-	public final static String BLUE_ERADICATED = "images/cureMarkers/blueEradicated.png";
-	public final static String RED_ERADICATED = "images/cureMarkers/redEradicated.png";
-	public final static String YELLOW_ERADICATED = "images/cureMarkers/yellowEradicated.png";
+	public final static String BLACK_OUTLINE = "/images/cureMarkers/blackOutline.png";
+	public final static String BLUE_OUTLINE = "/images/cureMarkers/blueOutline.png";
+	public final static String RED_OUTLINE = "/images/cureMarkers/redOutline.png";
+	public final static String YELLOW_OUTLINE = "/images/cureMarkers/yellowOutline.png";
+	public final static String BLACK_CURED = "/images/cureMarkers/black.png";
+	public final static String BLUE_CURED = "/images/cureMarkers/blue.png";
+	public final static String RED_CURED = "/images/cureMarkers/red.png";
+	public final static String YELLOW_CURED = "/images/cureMarkers/yellow.png";
+	public final static String BLACK_ERADICATED = "/images/cureMarkers/blackEradicated.png";
+	public final static String BLUE_ERADICATED = "/images/cureMarkers/blueEradicated.png";
+	public final static String RED_ERADICATED = "/images/cureMarkers/redEradicated.png";
+	public final static String YELLOW_ERADICATED = "/images/cureMarkers/yellowEradicated.png";
 	
 	public final static String PLAYER_DISCARD_PILE = "<html>Player Deck<br>" + "<center>Discard Pile</center></html>";
 }
--- src/main/java/game/GameView.java
+++ src/main/java/game/GameView.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import cards.CardView;
+import diseases.DiseaseView;
 
 import static constants.Constants.*;
 
@@ -65,7 +66,11 @@ public class GameView extends JFrame {
 	}
 	
 	private void drawCureOutlineMarkers() {
+		DiseaseView cureMarkers = new DiseaseView();
 		
+		cureMarkers.drawPanel();
+		
+		this.gameInfoPanel.add(cureMarkers);
 	}
 	
 	private void drawDiseaseInfoCounters() {
